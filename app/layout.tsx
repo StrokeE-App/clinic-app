dabccadcee70a6aaf7601eaae02b81c9bced700c
Andres Velasco <62601072+andrew921as@users.noreply.github.com>
2025-03-30 07:04:48 -0500

    Created the files to the PWA process (#4)

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
 import { Toaster } from "react-hot-toast";
 import { SseProvider } from "@/context/SseContext";
+import ServiceWorkerRegistration from "./components/ServiceWorkerRegistration";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,6 +19,17 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "StrokeE - Clinic App",
   description: "Emergency response system for Clinic",
+  manifest: '/manifest.json',
+	appleWebApp: {
+		capable: true,
+		statusBarStyle: 'default',
+		title: 'StrokeE',
+	},
+	viewport: {
+		width: 'device-width',
+		initialScale: 1,
+		maximumScale: 1,
+	},
 };
 
 export default function RootLayout({
@@ -27,9 +39,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
+      <head>
+				<link rel="apple-touch-icon" sizes="192x192" href="/strokee-192x192.png" />
+				<link rel="apple-touch-icon" sizes="512x512" href="/strokee-512x512.png" />
+				<meta name="apple-mobile-web-app-capable" content="yes" />
+				<meta name="apple-mobile-web-app-status-bar-style" content="default" />
+				<meta name="theme-color" content="#000000" />
+			</head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-customWhite`}
       >
+        <ServiceWorkerRegistration />
         <SseProvider>
           <AuthProvider>{children}</AuthProvider>
           <Toaster position="top-center" />
